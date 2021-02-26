9e3740a9444870fbcc1059b7c55d87364c658619
DatumMatrix <72856079+DatumMatrix@users.noreply.github.com>
2021-02-26 11:55:47 +0200

    Test

--- DataAccess/ReadData.java
+++ DataAccess/ReadData.java
@@ -1,8 +1,11 @@
+package DataAccess;
+
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.List;
+import BusinessLogicLayer.*;
 
 public class ReadData 
 {
@@ -17,7 +20,7 @@ public class ReadData
             //Split the fields up into an array     
             String[] SeperatedData = data.split(",");
             //Create Client Object
-            //clients.Add(new Client(seperatedData[0],seperatedData[1],seperatedData[2])) //Name, Surname, Phone Number
+            clients.Add(new Client(seperatedData[0],seperatedData[1],seperatedData[2])) //Name, Surname, Phone Number
 
             List<String> menu = new ArrayList<String>(); //A list that will contain all the menu options
             int offset = 0;                             //This is to help determine where the next field is after the list of menu items
@@ -31,7 +34,7 @@ public class ReadData
             }
 
             //Create Event Object
-            //events.Add(new Event(seperatedData[3],seperatedData[4],seperatedData[5],seperatedData[6],seperatedData[7],menu,seperatedData[8+offset],seperatedData[9+offset],seperatedData[10+offset]))
+            events.Add(new Event(seperatedData[3],seperatedData[4],seperatedData[5],seperatedData[6],seperatedData[7],menu,seperatedData[8+offset],seperatedData[9+offset],seperatedData[10+offset]))
             
             System.out.println(data);
           }

f8591f258f123c90e5d39d00bda0cee253653471
RucardokrugerBC <576344@student.belgiumcampus.ac.za>
2021-02-26 12:22:16 +0200

    }

--- BusinessLogicLayer/Wedding.java
+++ BusinessLogicLayer/Wedding.java
@@ -1,6 +1,46 @@
 package BusinessLogicLayer;
+import java.util.*;
 
-public class Wedding 
-{
+public class Wedding implements Events{
     
+    String eventType = "Wedding";
+    String eventDateandTime;
+    String eventAddress;
+    int numberOfAdults;
+    int numberOfChildren;
+    
+    //menu
+    String adultMeal;
+    String kidsMeal;
+    String Desert;
+
+	@Override
+	public void getdetails() 
+    {
+		
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Date & Time: ");
+        eventDateandTime = scn.nextLine();
+
+        System.out.println("Address: ");
+        eventAddress = scn.nextLine();
+
+        System.out.println("How many adults?");
+        numberOfAdults = scn.nextInt();
+
+        System.out.println("How many children?");
+        numberOfChildren = scn.nextInt();
+
+        System.out.println("==================================Menu:=============================");
+        System.out.println("Do you want kids preset menu? ");
+        adultMeal = scn.nextLine();
+
+        System.out.println("Do you want adult preset menu? ");
+        kidsMeal = scn.nextLine();
+
+        System.out.println("Do you want desert? ");
+        Desert = scn.nextLine();
+
+        scn.close();
+    }
 }

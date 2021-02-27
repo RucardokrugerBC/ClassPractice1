c13b97396e2668348d4c9392c5d5a967c62087b5
Maloka-Zapu <575857@student.belgiumcampus.ac.za>
2021-02-27 15:31:53 +0200

    write to textfile

---
+++ DataAccessLayer/WriteData.java
@@ -0,0 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class WriteData {
+
+   public void SaveBooking(String name, String surname, String phone,String eventType, String eventDateandTime, String eventCity, String eventArea, String eventStreet,
+   String eventTheme, int numberOfAdults, int numberOfChildren, String menu )
+   {
+    try {
+        FileWriter myWriter = new FileWriter("Booking.txt");
+        myWriter.write(name+","+surname+","+phone+","+eventType+","+eventDateandTime+","+eventCity+","+eventArea+","+eventStreet+","
+        +eventTheme+","+ numberOfAdults+","+numberOfChildren+","+menu);
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+   }
+}
